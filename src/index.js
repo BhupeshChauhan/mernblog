ee8e254bb34effad55bfd7800754214125b1f201
bhupesh-lirctek <erchauhanbhupesh@gmail.com>
2023-08-24 20:56:29 +0530

    Basic setup for webpack

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Create this component in the next step
+
+ReactDOM.render(<App />, document.getElementById('root'));
