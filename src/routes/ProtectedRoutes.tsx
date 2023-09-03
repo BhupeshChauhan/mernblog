78307522e41fe2080968804d33e30287e823353d
bhupesh-lirctek <122273526+bhupesh-lirctek@users.noreply.github.com>
2023-09-03 17:11:19 +0530

    Routes-settings

---
+++ src/routes/ProtectedRoutes.tsx
@@ -0,0 +1,30 @@
+// project import
+import React from 'react'
+import Home from "../pages/Home";
+import MainLayout from "../layout/MainLayout";
+import Contact from '../pages/Contact';
+import NotFound from '../pages/NotFound';
+
+
+// ==============================|| MAIN ROUTING ||============================== //
+
+const MainRoutes = {
+  path: '/',
+  element: <MainLayout />,
+  children: [
+    {
+      path: '/',
+      element: <Home />,
+    },
+    {
+      path: '/contact',
+      element: <Contact />,
+    },
+    {
+      path: '*',
+      element: <NotFound />,
+    },
+  ],
+};
+
+export default MainRoutes;
